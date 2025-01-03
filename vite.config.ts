92ff815d19bdae0f9cd8b1c50db584c3c9140b4f
Lavion <ilavion4u@gmail.com>
2025-01-03 22:57:55 +0100

    Using the FerMPY/react-ts-template

---
+++ vite.config.ts
@@ -0,0 +1,28 @@
+/// <reference types="vitest/config" />
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+const ReactCompilerConfig = {};
+
+const isTest = process.env.NODE_ENV === "test";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    tsconfigPaths(),
+    react({
+      babel: {
+        plugins: [["babel-plugin-react-compiler", ReactCompilerConfig]],
+      },
+    }),
+    !isTest && TanStackRouterVite(),
+  ],
+  test: {
+    globals: true,
+    isolate: true,
+    environment: "jsdom",
+    setupFiles: "./tests/setup.tsx",
+  },
+});
